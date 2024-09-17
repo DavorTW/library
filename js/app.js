ca17e1895cb1bcd7b2d66854d767dd65364e969e
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-09-17 16:42:24 +0800

    Added logic that handles addition of new books

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-const myLibrary = [
-    {
-        title: "the hobbit",
-        author: "JRR Tolkien",
-        pages: "345",
-        read: false,
-    }
-];
+const myLibrary = [];
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const numOfPages = document.querySelector("#numOfPages");
 const btn = document.querySelector(".add-book");
 const dialog = document.querySelector(".dialog-modal");
 const btnSubmit = document.querySelector(".submit");
+const tbody = document.querySelector("tbody");
+
 
 
 function Book(title, author, numOfPages, read){
@@ -25,9 +20,22 @@ function addBookToLibrary(title, author, numOfPages, read){
     myLibrary.push(new Book(title, author, numOfPages, read));
 }
 
-function createBookCard(){
 
+
+function showBookInTable(libraryArray){
+    tbody.textContent = "";
+    libraryArray.forEach(book => {
+        const tr = document.createElement("tr");
+        console.log("tr created")
+        tbody.appendChild(tr);
+        for(let value in book){
+            const td = document.createElement("td");
+            td.textContent = book[value];
+            tr.appendChild(td);
+        }
+    });
 }
+
 
 btn.addEventListener("click", () => {
     dialog.showModal();
@@ -37,6 +45,7 @@ btnSubmit.addEventListener("click", (e) => {
     e.preventDefault();
     const read = document.querySelector('input[name="status"]:checked').value;
     addBookToLibrary(title.value, author.value, numOfPages.value, read);
+    showBookInTable(myLibrary);
     dialog.close();
 })
 
